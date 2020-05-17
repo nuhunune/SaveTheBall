b40acfa7ece50af3fffe9e7be6a6f7911d39b22f
nuhunune <nuhunune@gmail.com>
2020-05-17 18:40:13 +0900

    Cleaned code and created classes.

--- Game/SaveTheBall(tmp)/Main.cpp
+++ Game/SaveTheBall(tmp)/Main.cpp
@@ -1,6 +1,6 @@
-﻿
-# include <Siv3D.hpp> // OpenSiv3D v0.4.1
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.4.1
 
+//メイン関数
 void Main()
 {
 	while (System::Update())
@@ -8,3 +8,25 @@ void Main()
 
 	}
 }
+
+//各シーンを表す列挙型
+enum Scenes {
+
+};
+//現在のシーン
+Scenes scene;
+
+//タイトル表示用クラス
+class Title {
+
+};
+
+//結果表示用クラス
+class Result {
+
+};
+
+//ゲームの本体クラス
+class Game {
+
+};
