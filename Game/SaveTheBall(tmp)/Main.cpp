9f3bd7515d2bdfbc47c840f2545409781bbd3b94
nuhunune <nuhunune@gmail.com>
2020-05-18 20:25:42 +0900

    Created class Ball and added some params.

--- Game/SaveTheBall(tmp)/Main.cpp
+++ Game/SaveTheBall(tmp)/Main.cpp
@@ -33,5 +33,18 @@ class Result {
 
 //ゲームの本体クラス
 class Game {
+	struct Balls//各球を表す構造体
+	{
+		double x;//現在のx座標
+		double y;//現在のy座標
+		double radius;//半径
+		double moveByX;//1フレーム当たりのx座標の増分
+		double moveByY;//1フレーム当たりのy座標の増分
+		Circle circle;
+		double distance(Balls ball1, Balls ball2) {//与えられた2つのボールの距離を返す関数
+			return std::sqrt((ball1.x - ball2.x) * (ball1.x - ball2.x) + (ball1.y - ball2.y) * (ball1.y - ball2.y));
+		}
+	};
+	
 
 };
