d879acbb01119d0ab098e472d4e452e402440eca
nuhunune <nuhunune@gmail.com>
2020-05-19 21:02:22 +0900

    Added rough process to Ball::update function.

--- Game/SaveTheBall(tmp)/Main.cpp
+++ Game/SaveTheBall(tmp)/Main.cpp
@@ -26,6 +26,9 @@ class Result {
 
 //ゲームの本体クラス
 class Game {
+	static constexpr int ballSize = 100;//ボールの大きさ(すべて同じ)
+	static constexpr int rightEdgeOfMainScreen=1200, leftEdgeOfMainScreen=0, topEdgeOfMainScreen=0, bottomEdgeOfMainScreen=900;
+
 	class ComplexNumber {//複素数を扱うクラス
 	private:
 	public:
@@ -35,6 +38,7 @@ class Game {
 			this->a = a;
 			this->b = b;
 		}
+		//5/19 20:32 static全部なくていいようにした方がいいかも
 		static double abs(ComplexNumber* arg) {//絶対値
 			return std::sqrt(((*arg).a) * ((*arg).a)+ ((*arg).b)* ((*arg).b));
 		}
@@ -72,26 +76,29 @@ class Game {
 	};
 	class Ball//各球を扱うクラス
 	{
-	private:
 		double x;//現在のx座標
 		double y;//現在のy座標
-		double radius;//半径
 		double moveByX;//1フレーム当たりのx座標の増分
 		double moveByY;//1フレーム当たりのy座標の増分
-		Circle circle;
-		double distance(Ball* ball1, Ball* ball2) {//与えられた2つのボールの距離を返す関数
+		static double distance(Ball* ball1, Ball* ball2) {//与えられた2つのボールの距離を返す関数
 			return std::sqrt(((*ball1).x - (*ball2).x) * ((*ball1).x - (*ball2).x) + ((*ball1).y - (*ball2).y) * ((*ball1).y - (*ball2).y));
 		}
-		void changeDirection(Ball* ball,double newMoveByX,double newMoveByY) {//1フレーム当たりの増分の変更
-			(*ball).moveByX = newMoveByX;
-			(*ball).moveByY = newMoveByY;
+		void changeDirection(double newMoveByX,double newMoveByY) {//1フレーム当たりの増分の変更
+			moveByX = newMoveByX;
+			moveByY = newMoveByY;
 		}
 		
-		void changeTheDirectionOfBumpedBalls(Ball* ball1, Ball* ball2) {//衝突した2つのボールの方向を変更　引数の2つのボールがぶつかっている前提
+		static void changeTheDirectionOfBumpedBalls(Ball* ball1, Ball* ball2) {//衝突した2つのボールの方向を変更　引数の2つのボールがぶつかっている前提
 			//複素数を実装してからやる
 		}
 
 	public:
+		Ball(double x,double y,double moveByX,double moveByY) {
+			this->x = x;
+			this->y = y;
+			this->moveByX = moveByX;
+			this->moveByY = moveByY;
+		}
 		void update() {
 			/*
 			最初に壁の反射
@@ -99,13 +106,40 @@ class Game {
 			↑moveByを変えるだけ
 			次にボールの位置を変える
 			描画をいつやるかは未定
-			
+			注：二つ以上の衝突が同じボールに対して同時に起こると多分バグる
 			*/
+			const int numberOfBalls = balls.size();//処理するボールの個数
+			for (int i = 0; i < numberOfBalls; i++) {//画面端への衝突時の方向変更
+				if (balls[i].x <= leftEdgeOfMainScreen+ballSize) {//左端に衝突するとき
+					balls[i].changeDirection(-balls[i].moveByX, balls[i].moveByY);//x軸方向の速さをマイナス一倍
+				}
+				if (balls[i].x >= rightEdgeOfMainScreen - ballSize) {//右端に衝突するとき
+					balls[i].changeDirection(-balls[i].moveByX, balls[i].moveByY);//x軸方向の速さをマイナス一倍
+				}
+				if (balls[i].y <= topEdgeOfMainScreen + ballSize) {//上端に衝突するとき
+					balls[i].changeDirection(balls[i].moveByX, -balls[i].moveByY);//y軸方向の速さをマイナス一倍
+				}
+				if (balls[i].y >= bottomEdgeOfMainScreen - ballSize) {
+					balls[i].changeDirection(balls[i].moveByX, -balls[i].moveByY);//y軸方向の速さをマイナス一倍
+				}
+			}
+			for (int i = 0; i < numberOfBalls; i++) {//ボール同士の衝突時の方向変更
+				for (int j = i + 1; j < numberOfBalls; j++) {//同じ衝突を二度見ないようにする
+					if (Ball::distance(&balls[i], &balls[j]) <= ballSize * 2) {
+						Ball::changeTheDirectionOfBumpedBalls(&balls[i], &balls[j]);
+					}
+				}
+			}
+			for (int i = 0; i < numberOfBalls; i++) {//ボール位置の変更
+				balls[i].x = balls[i].x + balls[i].moveByX;
+				balls[i].y = balls[i].y + balls[i].moveByY;
+			}
+
 		}
 		
 	};
 public:
-	Array<Ball> balls;
+	static Array<Ball> balls;
 	//static void cNumTest(double a1, double b1, double a2, double b2) {//複素数クラスのテスト用
 	//	ComplexNumber c1(a1, b1);
 	//	ComplexNumber c2(a2, b2);
@@ -114,6 +148,9 @@ public:
 	//	Print << c1.show() << U"*" << c2.show() << U"=" << ComplexNumber::multiplication(&c1, &c2).show();
 	//	Print << c1.show() << U"/" << c2.show() << U"=" << ComplexNumber::dividion(&c1, &c2).show();
 	//}
+	static void addBall() {//ballのテスト用
+		balls << Ball(600, 450, 30, 30);
+	}
 	
 
 };
@@ -121,6 +158,8 @@ public:
 //メイン関数
 void Main()
 {
+	Window::Resize(Size(1200,900));
+	Game::addBall();
 
 	while (System::Update())
 	{
