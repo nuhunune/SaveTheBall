9d12c06c291a8c53602b8d7fc73749cf6c789fc9
nuhunune <nuhunune@gmail.com>
2020-05-20 19:32:19 +0900

    Finished to make struct Ball.

--- Game/SaveTheBall(tmp)/Main.cpp
+++ Game/SaveTheBall(tmp)/Main.cpp
@@ -1,7 +1,5 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.4.1
 
-
-
 //各シーンを表す列挙型
 enum Scenes {
 	PLAYING_GAME//ゲーム中
@@ -27,11 +25,10 @@ class Result {
 //ゲームの本体クラス
 class Game {
 	static constexpr int ballSize = 100;//ボールの大きさ(すべて同じ)
+	static constexpr double speedOfBalls = 15;//1フレーム当たりのボールの移動距離(すべて同じ)
 	static constexpr int rightEdgeOfMainScreen=1200, leftEdgeOfMainScreen=0, topEdgeOfMainScreen=0, bottomEdgeOfMainScreen=900;
 
-	class ComplexNumber {//複素数を扱うクラス
-	private:
-	public:
+	struct ComplexNumber {//複素数を扱う構造体
 		//扱う複素数はiを虚数単位としてa+biとする
 		double a,b;
 		ComplexNumber(double a, double b) {//実部と虚部を引数にとるコンストラクタ
@@ -56,6 +53,9 @@ class Game {
 			double b2 = rightOperand.b;
 			return ComplexNumber(a * a2 - b * b2, a * b2 + a2 * b);
 		}
+		ComplexNumber operator*(double rightOperand) {//複素数の定数倍
+			return ComplexNumber(a * rightOperand, b * rightOperand);
+		}
 		ComplexNumber operator/(ComplexNumber rightOperand) {//割り算(arg1/arg2)
 			double absOfRightOperand = rightOperand.abs();
 			ComplexNumber reciprocalOfRightOperand = ComplexNumber(rightOperand.a / (absOfRightOperand * absOfRightOperand), -rightOperand.b / (absOfRightOperand * absOfRightOperand));//右オペランドの逆数
@@ -72,7 +72,7 @@ class Game {
 
 
 	};
-	class Ball//各球を扱うクラス
+	struct Ball//各球を扱う構造体
 	{
 		double x;//現在のx座標
 		double y;//現在のy座標
@@ -88,56 +88,68 @@ class Game {
 		
 		static void changeTheDirectionOfBumpedBalls(Ball* ball1, Ball* ball2) {//衝突した2つのボールの方向を変更　引数の2つのボールがぶつかっている前提
 			//複素数を実装してからやる
+			double x1 = (*ball1).x;//それぞれの玉の座標
+			double y1 = (*ball1).y;
+			double x2 = (*ball2).x;
+			double y2 = (*ball2).y;
+			double moveByX1 = (*ball1).moveByX;//それぞれの円の1フレーム当たりの座標の増分
+			double moveByY1 = (*ball1).moveByY;
+			double moveByX2 = (*ball2).moveByX;
+			double moveByY2 = (*ball2).moveByY;
+			ComplexNumber tangentLine=ComplexNumber(x1-x2,y1-y2)*ComplexNumber(0,1);//2つの円の接線と同じ偏角の複素数
+			ComplexNumber directionOfBall1 = ComplexNumber(moveByX1,moveByY1);//2つの円の進行方向を表す複素数
+			ComplexNumber directionOfBall2 = ComplexNumber(moveByX2, moveByY2);
+			ComplexNumber newDirectionOfBall1 = (tangentLine * tangentLine / directionOfBall1).unitVector()*speedOfBalls;
+			ComplexNumber newDirectionOfBall2 = (tangentLine * tangentLine / directionOfBall2).unitVector()*speedOfBalls;
+			(*ball1).moveByX = newDirectionOfBall1.a;
+			(*ball1).moveByY = newDirectionOfBall1.b;
+			(*ball2).moveByX = newDirectionOfBall2.a;
+			(*ball2).moveByY = newDirectionOfBall2.b;
 		}
 
-	public:
+	
 		Ball(double x,double y,double moveByX,double moveByY) {
 			this->x = x;
 			this->y = y;
 			this->moveByX = moveByX;
 			this->moveByY = moveByY;
 		}
-		void update() {
-			/*
-			最初に壁の反射
-			次にfor回してボール同士の反射
-			↑moveByを変えるだけ
-			次にボールの位置を変える
-			描画をいつやるかは未定
-			注：二つ以上の衝突が同じボールに対して同時に起こると多分バグる
-			*/
-			const int numberOfBalls = (int)balls.size();//処理するボールの個数
-			for (int i = 0; i < numberOfBalls; i++) {//画面端への衝突時の方向変更
-				if (balls[i].x <= leftEdgeOfMainScreen+ballSize) {//左端に衝突するとき
-					balls[i].changeDirection(-balls[i].moveByX, balls[i].moveByY);//x軸方向の速さをマイナス一倍
-				}
-				if (balls[i].x >= rightEdgeOfMainScreen - ballSize) {//右端に衝突するとき
-					balls[i].changeDirection(-balls[i].moveByX, balls[i].moveByY);//x軸方向の速さをマイナス一倍
-				}
-				if (balls[i].y <= topEdgeOfMainScreen + ballSize) {//上端に衝突するとき
-					balls[i].changeDirection(balls[i].moveByX, -balls[i].moveByY);//y軸方向の速さをマイナス一倍
-				}
-				if (balls[i].y >= bottomEdgeOfMainScreen - ballSize) {
-					balls[i].changeDirection(balls[i].moveByX, -balls[i].moveByY);//y軸方向の速さをマイナス一倍
-				}
-			}
-			for (int i = 0; i < numberOfBalls; i++) {//ボール同士の衝突時の方向変更
-				for (int j = i + 1; j < numberOfBalls; j++) {//同じ衝突を二度見ないようにする
-					if (Ball::distance(&balls[i], &balls[j]) <= ballSize * 2) {
-						Ball::changeTheDirectionOfBumpedBalls(&balls[i], &balls[j]);
-					}
-				}
-			}
-			for (int i = 0; i < numberOfBalls; i++) {//ボール位置の変更
-				balls[i].x = balls[i].x + balls[i].moveByX;
-				balls[i].y = balls[i].y + balls[i].moveByY;
-			}
-
-		}
-		
 	};
+	void updateBall() {//ボールの位置の更新と描画
+		const int32 numberOfBalls = balls.size();//処理するボールの個数
+		for (int i = 0; i < numberOfBalls; i++) {//画面端への衝突時の方向変更
+			if (balls[i].x <= leftEdgeOfMainScreen + ballSize) {//左端に衝突するとき
+				balls[i].changeDirection(-balls[i].moveByX, balls[i].moveByY);//x軸方向の速さをマイナス一倍
+			}
+			if (balls[i].x >= rightEdgeOfMainScreen - ballSize) {//右端に衝突するとき
+				balls[i].changeDirection(-balls[i].moveByX, balls[i].moveByY);//x軸方向の速さをマイナス一倍
+			}
+			if (balls[i].y <= topEdgeOfMainScreen + ballSize) {//上端に衝突するとき
+				balls[i].changeDirection(balls[i].moveByX, -balls[i].moveByY);//y軸方向の速さをマイナス一倍
+			}
+			if (balls[i].y >= bottomEdgeOfMainScreen - ballSize) {
+				balls[i].changeDirection(balls[i].moveByX, -balls[i].moveByY);//y軸方向の速さをマイナス一倍
+			}
+		}
+		for (int i = 0; i < numberOfBalls; i++) {//ボール同士の衝突時の方向変更
+			for (int j = i + 1; j < numberOfBalls; j++) {//同じ衝突を二度見ないようにする
+				if (Ball::distance(&balls[i], &balls[j]) <= ballSize * 2) {
+					Ball::changeTheDirectionOfBumpedBalls(&balls[i], &balls[j]);
+				}
+			}
+		}
+		for (int i = 0; i < numberOfBalls; i++) {//ボール位置の変更
+			balls[i].x = balls[i].x + balls[i].moveByX;
+			balls[i].y = balls[i].y + balls[i].moveByY;
+		}
+		/*Print << balls[0].moveByX;
+		Print << balls[0].moveByY;*/
+		for (int i = 0; i < numberOfBalls; i++) {
+			Circle(balls[i].x, balls[i].y, ballSize).draw(Palette::White);
+		}
+	}
 public:
-	static Array<Ball> balls;
+	Array<Ball> balls;
 	//static void cNumTest(double a1, double b1, double a2, double b2) {//複素数クラスのテスト用
 	//	ComplexNumber c1(a1, b1);
 	//	ComplexNumber c2(a2, b2);
@@ -146,19 +158,25 @@ public:
 	//	Print << c1.show() << U"*" << c2.show() << U"=" << (c1*c2).show();
 	//	Print << c1.show() << U"/" << c2.show() << U"=" << (c1/c2).show();
 	//}
-	static void addBall() {//ballのテスト用
-		balls << Ball(600, 450, 30, 30);
+	void update(){//ゲームの更新
+		updateBall();
 	}
-	
+	void test() {//ボールのテスト用
+		balls << Ball(800, 450, 2, 3);
+		balls << Ball(400, 450, -2, 4);
+
+	}
 
 };
 
 //メイン関数
 void Main()
 {
-	Window::Resize(Size(1200,900));
-	while (System::Update())
+	Window::Resize(Size(1200,900));//windowのサイズは1200x900にする(あとで上の方に定数用意するかも)
+	Game game;//ゲーム本体の本体
+	game.test();
+	while (System::Update())//switch文で分岐しよう
 	{
-
+		game.update();
 	}
 }
